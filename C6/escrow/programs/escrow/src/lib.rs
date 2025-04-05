5d672604621286e507e28879aec670028bbe4e5e
DariusBurnete <dariusburnete@yahoo.com>
2025-04-06 01:52:15 +0300

    C6

---
+++ C6/escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,22 @@
+use anchor_lang::prelude::*;
+
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("BbZeCjcEEsv12cssSQcjy9XLpMwSrbvJQs3EyrJtzTEA");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make_offer(ctx: Context<MakeOffer>, id: u64, token_a_amount: u64, token_b_wanted_amount: u64) -> Result<()> {
+        make_offer::handler(ctx, id, token_a_amount, token_b_wanted_amount)
+    }
+    
+}
---
+++ C6/escrow/programs/escrow/src/state/offer.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct Offer {
+    pub id: u64,
+    pub maker: Pubkey,
+    pub token_a_amount: u64,
+    pub token_a_mint: Pubkey,
+    pub token_b_wanted_amount: u64,
+    pub token_b_mint: Pubkey,
+}
